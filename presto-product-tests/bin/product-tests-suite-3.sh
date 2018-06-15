685059a6b4b23cceb072fcb9f25a4575b03c63da
Raphael CLAUDE <r.claude@criteo.com>
2018-06-15 12:47:55 -0400

    Criteo specific TravisCI config
    * Slack integration
    * Run on criteo branches
    * Avoid waisting cpu cycles on unused/unmodified features

--- presto-product-tests/bin/product-tests-suite-3.sh
+++ presto-product-tests/bin/product-tests-suite-3.sh
@@ -2,25 +2,25 @@
 
 set -xeuo pipefail
 
-presto-product-tests/bin/run_on_docker.sh \
-    singlenode-mysql \
-    -g mysql_connector,mysql
+#presto-product-tests/bin/run_on_docker.sh \
+#    singlenode-mysql \
+#    -g mysql_connector,mysql
 
-presto-product-tests/bin/run_on_docker.sh \
-    singlenode-postgresql \
-    -g postgresql_connector
+#presto-product-tests/bin/run_on_docker.sh \
+#    singlenode-postgresql \
+#    -g postgresql_connector
 
-presto-product-tests/bin/run_on_docker.sh \
-    singlenode-cassandra \
-    -g cassandra
+#presto-product-tests/bin/run_on_docker.sh \
+#    singlenode-cassandra \
+#    -g cassandra
 
 presto-product-tests/bin/run_on_docker.sh \
     singlenode-kerberos-hdfs-impersonation-with-wire-encryption \
     -g storage_formats,cli,hdfs_impersonation,authorization
 
-presto-product-tests/bin/run_on_docker.sh \
-    singlenode-kerberos-kms-hdfs-no-impersonation \
-    -g storage_formats
+#presto-product-tests/bin/run_on_docker.sh \
+#    singlenode-kerberos-kms-hdfs-no-impersonation \
+#    -g storage_formats
 
 # TODO enable avro when adding Metastore impersonation
 presto-product-tests/bin/run_on_docker.sh \
