f07b5b428c7f20fa976c6f9110b7afd25adef051
Raphael CLAUDE <r.claude@criteo.com>
2019-10-30 17:20:12 +0100

    Support catalog props in FileSessionPropertyManager

--- presto-session-property-managers/src/main/java/io/prestosql/plugin/session/SessionMatchSpec.java
+++ presto-session-property-managers/src/main/java/io/prestosql/plugin/session/SessionMatchSpec.java
@@ -14,6 +14,7 @@
 package io.prestosql.plugin.session;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
@@ -35,7 +36,32 @@ public class SessionMatchSpec
     private final Set<String> clientTags;
     private final Optional<String> queryType;
     private final Optional<Pattern> resourceGroupRegex;
-    private final Map<String, String> sessionProperties;
+    private final SessionProperties sessionProperties;
+
+    public static class SessionProperties
+    {
+        static final SessionProperties empty = new SessionProperties(ImmutableMap.of(), ImmutableMap.of());
+        private final Map<String, String> generalProperties;
+        private final Map<String, Map<String, String>> catalogsProperties;
+
+        public SessionProperties(
+                Map<String, String> generalProperties,
+                Map<String, Map<String, String>> catalogsProperties)
+        {
+            this.generalProperties = generalProperties;
+            this.catalogsProperties = catalogsProperties;
+        }
+
+        public Map<String, String> getGeneralProperties()
+        {
+            return generalProperties;
+        }
+
+        public Map<String, Map<String, String>> getCatalogsProperties()
+        {
+            return catalogsProperties;
+        }
+    }
 
     @JsonCreator
     public SessionMatchSpec(
@@ -44,7 +70,8 @@ public class SessionMatchSpec
             @JsonProperty("clientTags") Optional<List<String>> clientTags,
             @JsonProperty("queryType") Optional<String> queryType,
             @JsonProperty("group") Optional<Pattern> resourceGroupRegex,
-            @JsonProperty("sessionProperties") Map<String, String> sessionProperties)
+            @JsonProperty("sessionProperties") Map<String, String> sessionProperties,
+            @JsonProperty("catalogSessionProperties") Map<String, Map<String, String>> catalogSessionProperties)
     {
         this.userRegex = requireNonNull(userRegex, "userRegex is null");
         this.sourceRegex = requireNonNull(sourceRegex, "sourceRegex is null");
@@ -52,34 +79,38 @@ public class SessionMatchSpec
         this.clientTags = ImmutableSet.copyOf(clientTags.orElse(ImmutableList.of()));
         this.queryType = requireNonNull(queryType, "queryType is null");
         this.resourceGroupRegex = requireNonNull(resourceGroupRegex, "resourceGroupRegex is null");
-        requireNonNull(sessionProperties, "sessionProperties is null");
-        this.sessionProperties = ImmutableMap.copyOf(sessionProperties);
+        if (sessionProperties == null && catalogSessionProperties == null) {
+            throw new NullPointerException("sessionProperties and catalogSessionProperties are null");
+        }
+        this.sessionProperties = new SessionProperties(
+                (sessionProperties == null) ? ImmutableMap.of() : ImmutableMap.copyOf(sessionProperties),
+                (catalogSessionProperties == null) ? ImmutableMap.of() : ImmutableMap.copyOf(catalogSessionProperties));
     }
 
-    public Map<String, String> match(SessionConfigurationContext context)
+    public SessionProperties match(SessionConfigurationContext context)
     {
         if (userRegex.isPresent() && !userRegex.get().matcher(context.getUser()).matches()) {
-            return ImmutableMap.of();
+            return SessionProperties.empty;
         }
         if (sourceRegex.isPresent()) {
             String source = context.getSource().orElse("");
             if (!sourceRegex.get().matcher(source).matches()) {
-                return ImmutableMap.of();
+                return SessionProperties.empty;
             }
         }
         if (!clientTags.isEmpty() && !context.getClientTags().containsAll(clientTags)) {
-            return ImmutableMap.of();
+            return SessionProperties.empty;
         }
 
         if (queryType.isPresent()) {
             String contextQueryType = context.getQueryType().orElse("");
             if (!queryType.get().equalsIgnoreCase(contextQueryType)) {
-                return ImmutableMap.of();
+                return SessionProperties.empty;
             }
         }
 
         if (resourceGroupRegex.isPresent() && !resourceGroupRegex.get().matcher(context.getResourceGroupId().toString()).matches()) {
-            return ImmutableMap.of();
+            return SessionProperties.empty;
         }
 
         return sessionProperties;
@@ -115,9 +146,21 @@ public class SessionMatchSpec
         return resourceGroupRegex;
     }
 
-    @JsonProperty
-    public Map<String, String> getSessionProperties()
+    @JsonIgnore
+    public SessionProperties getAllSessionProperties()
     {
         return sessionProperties;
+    }
+
+    @JsonProperty("sessionProperties")
+    public Map<String, String> getSessionProperties()
+    {
+        return sessionProperties.generalProperties;
+    }
+
+    @JsonProperty("catalogSessionProperties")
+    public Map<String, Map<String, String>> getCatalogSessionProperties()
+    {
+        return sessionProperties.catalogsProperties;
     }
 }
